eb81f184596d74b0003fd36a862d0674f0769252
artprasetya <artiyan.prasetya@payfazz.com>
2020-12-13 11:40:34 +0700

    Add app bar on pdf viewer

--- lib/page/viewer/view.dart
+++ lib/page/viewer/view.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_full_pdf_viewer/flutter_full_pdf_viewer.dart';
-import 'package:print_to_pdf/global/components/template_page.dart';
 
 class PdfViewerView extends StatelessWidget {
   final String fileName;
@@ -14,9 +14,16 @@ class PdfViewerView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TemplatePage(
-      title: fileName,
-      body: PDFViewerScaffold(path: path),
-    );
+    return PDFViewerScaffold(
+        appBar: AppBar(
+          title: Text(fileName),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.share),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        path: path);
   }
 }
